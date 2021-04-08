8ff20dffb0f6e78e345238e7a5ec8797f26fc7c7
Artur Vaganian <armay@yandex.ru>
2021-04-09 01:14:29 +0300

    Make utility functions top-level

--- lib/src/main/kotlin/com/github/armay/okrandomaccess/OkRandomAccess.kt
+++ lib/src/main/kotlin/com/github/armay/okrandomaccess/OkRandomAccess.kt
@@ -3,26 +3,21 @@ package com.github.armay.okrandomaccess
 import java.nio.file.Path
 
 /**
- * This object provides convenient extensions for creation of random access sinks & sources.
+ * Creates a new random access file sink from a given path.
  */
-object OkRandomAccess {
-    /**
-     * Creates a new random access file sink from a given path.
-     */
-    fun Path.randomAccessSink(): RandomAccessSink = RandomAccessFileSink(this)
+fun Path.randomAccessSink(): RandomAccessSink = RandomAccessFileSink(this)
 
-    /**
-     * Creates a new random access file source from a given path.
-     */
-    fun Path.randomAccessSource(): RandomAccessSource = RandomAccessFileSource(this)
+/**
+ * Creates a new random access file source from a given path.
+ */
+fun Path.randomAccessSource(): RandomAccessSource = RandomAccessFileSource(this)
 
-    /**
-     * Creates a new buffered random access sink from a given random access sink.
-     */
-    fun RandomAccessSink.buffer() = RandomAccessBufferedSink(this)
+/**
+ * Creates a new buffered random access sink from a given random access sink.
+ */
+fun RandomAccessSink.buffer() = RandomAccessBufferedSink(this)
 
-    /**
-     * Creates a new buffered random access source from a given random access source.
-     */
-    fun RandomAccessSource.buffer() = RandomAccessBufferedSource(this)
-}
+/**
+ * Creates a new buffered random access source from a given random access source.
+ */
+fun RandomAccessSource.buffer() = RandomAccessBufferedSource(this)
